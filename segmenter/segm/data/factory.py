d651ee4069f49681297e71231935b5e94de69c58
class54 <class54@access3.wexac.weizmann.ac.il>
2023-02-02 15:17:56 +0200

    fix import of PannukeDataset

--- segmenter/segm/data/factory.py
+++ segmenter/segm/data/factory.py
@@ -5,7 +5,7 @@ from segm.data import ADE20KSegmentation
 from segm.data import PascalContextDataset
 from segm.data import CityscapesDataset
 from segm.data import Loader
-from pannuke import PannukeDataset
+from segm.data import PannukeDataset
 
 
 def create_dataset(dataset_kwargs):
