a489b6f6e0f88bf38b2a2c3b6f31e553860104ac
Nitzan Haim <61940253+nitzan-haim@users.noreply.github.com>
2023-02-14 13:21:14 +0200

    Train pannuke for the first time ever on the GPU (#7)

--- segmenter/segm/data/config/LoadAnnotationsPannuke.py
+++ segmenter/segm/data/config/LoadAnnotationsPannuke.py
@@ -2,7 +2,7 @@ import os.path as osp
 
 import mmcv
 import numpy as np
-import pycocotools.mask as maskUtils
+# import pycocotools.mask as maskUtils
 
 # from mmdet.core import BitmapMasks, PolygonMasks
 
@@ -92,30 +92,30 @@ class LoadAnnotationsPannuke:
         results['gt_labels'] = results['ann_info']['labels'].copy()
         return results
 
-    def _poly2mask(self, mask_ann, img_h, img_w):
-        """Private function to convert masks represented with polygon to
-        bitmaps.
-        Args:
-            mask_ann (list | dict): Polygon mask annotation input.
-            img_h (int): The height of output mask.
-            img_w (int): The width of output mask.
-        Returns:
-            numpy.ndarray: The decode bitmap mask of shape (img_h, img_w).
-        """
-
-        if isinstance(mask_ann, list):
-            # polygon -- a single object might consist of multiple parts
-            # we merge all parts into one mask rle code
-            rles = maskUtils.frPyObjects(mask_ann, img_h, img_w)
-            rle = maskUtils.merge(rles)
-        elif isinstance(mask_ann['counts'], list):
-            # uncompressed RLE
-            rle = maskUtils.frPyObjects(mask_ann, img_h, img_w)
-        else:
-            # rle
-            rle = mask_ann
-        mask = maskUtils.decode(rle)
-        return mask
+#    def _poly2mask(self, mask_ann, img_h, img_w):
+#        """Private function to convert masks represented with polygon to
+#        bitmaps.
+#        Args:
+#            mask_ann (list | dict): Polygon mask annotation input.
+#            img_h (int): The height of output mask.
+#            img_w (int): The width of output mask.
+#        Returns:
+#            numpy.ndarray: The decode bitmap mask of shape (img_h, img_w).
+#        """
+#
+#        if isinstance(mask_ann, list):
+#            # polygon -- a single object might consist of multiple parts
+#            # we merge all parts into one mask rle code
+#            rles = maskUtils.frPyObjects(mask_ann, img_h, img_w)
+#            rle = maskUtils.merge(rles)
+#        elif isinstance(mask_ann['counts'], list):
+#            # uncompressed RLE
+#            rle = maskUtils.frPyObjects(mask_ann, img_h, img_w)
+#        else:
+#            # rle
+#            rle = mask_ann
+#        mask = maskUtils.decode(rle)
+#        return mask
 
     def process_polygons(self, polygons):
         """Convert polygons to list of ndarray and filter invalid polygons.
